3f398a5da5a006cc8daf44fbe65922618da3d981
dataoshark-spec <dataoshark@gmail.com>
2026-07-19 16:03:35 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker — 記帳 LEDGER 個人版
 
-const CACHE_VERSION = 'ledger-1150716M';
+const CACHE_VERSION = 'ledger-1150719A';
 const CACHE_NAME = `${CACHE_VERSION}-cache`;
 
 const PRECACHE_URLS = [
